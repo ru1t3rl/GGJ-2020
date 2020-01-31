47dd788211a5b90aa18862378f6ac4f7f3b3149c
Quintin Yu <quintinyu02072@gmail.com>
2020-01-31 21:39:22 +0100

    Enemies
    Added: base Enemy
    Added: Trojan Enemy base
    
    ToDo: enemy should do damage to player and base

---
+++ Assets/Scripts/Trojan_Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trojan_Enemy : Core_Enemy
+{
+
+    private void Awake()
+    {
+        SetMaxHealth(50f);    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxSpeed = 5f;
+        visionRange = 100f;
+
+        health = GetMaxhealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Virus has: " + health + " Health left");
+
+        MoveObject();
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TakeDamage(10f);
+        }
+    }
+
+    public override void MoveObject()
+    {
+        base.MoveObject();
+    }
+
+    public override void TrackTarget(GameObject _object)
+    {
+        base.TrackTarget(_object);
+    }
+
+    public override void TakeDamage(float _damage)
+    {
+        base.TakeDamage(_damage);
+    }
+
+    public override void Die()
+    {
+        base.Die();
+    }
+}
