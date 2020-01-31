431698abd3d33e1bac782c681fc73b70db330d55
Quintin Yu <quintinyu02072@gmail.com>
2020-01-31 23:54:52 +0100

    Trojan

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameObject sPlayer;
+    [SerializeField] GameObject player;
+
+    static GameObject sCPU;
+    [SerializeField] GameObject cPU;
+
+    private void Awake()
+    {
+        sPlayer = player;
+        sCPU = cPU;
+    }
+
+    public static GameObject Player { get => sPlayer; }
+    public static GameObject CPU { get => sCPU; }
+}
