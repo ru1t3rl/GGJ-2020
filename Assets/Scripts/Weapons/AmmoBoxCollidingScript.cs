371369c40dc769267db67e182424b752e7d5ae15
Keijser99 <ninokeijser@hotmail.com>
2020-02-01 17:11:11 +0100

    The prefab of the ammo box now contains a script to make it able to be picked up by the player and refills ammo

---
+++ Assets/Scripts/Weapons/AmmoBoxCollidingScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBoxCollidingScript : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("AMMMMOOOOOO");
+            other.GetComponent<Player>().gun.Reload();
+            gameObject.SetActive(false);
+        }
+    }
+}
