c02d39af9cc9b03a130b97c3ee880c2c487d3ea9
Keijser99 <ninokeijser@hotmail.com>
2020-02-02 03:17:55 +0100

    Added a Feedback for when you pick up a ammo box

--- Assets/Scripts/Weapons/AmmoBoxCollidingScript.cs
+++ Assets/Scripts/Weapons/AmmoBoxCollidingScript.cs
@@ -2,45 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AmmoBoxCollidingScript : MonoBehaviour
 {
-    [SerializeField] private AudioClip PickupSFX;
-    [SerializeField] private GameObject AmmoPickupAmount;
+    [SerializeField] AudioClip PickupSFX;
+    [SerializeField] TextMeshProUGUI AmmoPickupAmount;
     private int FeedbackDuration = 120;
     private int FeedbackCounter;
 
+    private bool looting;
+    public float upTime;
+    public float speed;
+
+    [SerializeField] private Renderer rend;
+    [SerializeField] private Collider col;
+
+    [SerializeField] private GameObject[] AmmoBox;
+
     private void Start()
     {
+        rend = GetComponent<Renderer>();
+        col = GetComponent<Collider>();
+
         gameObject.SetActive(true);
-        AmmoPickupAmount.SetActive(false);
+        AmmoPickupAmount.gameObject.SetActive(false);
 
         FeedbackCounter = 0;
+        looting = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            rend.enabled = false;
+            col.enabled = false;
+
+            foreach (GameObject components in AmmoBox)
+            {
+                components.SetActive(false);
+            }
+
             AudioManager.Instance.PlaySFX(PickupSFX);
             other.GetComponent<Player>().gun.Reload();
 
-            Feedback();
-
-            gameObject.SetActive(false);
+            if (!looting)
+                StartCoroutine(FeedbackTimer(upTime));
+            else
+            {
+                AmmoPickupAmount.transform.position += new Vector3(0, speed, 0);
+            }            
         }
     }
 
-    private void Feedback()
+    IEnumerator FeedbackTimer(float time)
     {
-        AmmoPickupAmount.SetActive(true);
-        FeedbackCounter++;
-        Debug.Log(FeedbackCounter);
-
-        if (FeedbackCounter >= FeedbackDuration)
-        {
-            Debug.Log("Feedback Completed");
-            AmmoPickupAmount.SetActive(false);
-        }
+        looting = true;
+        AmmoPickupAmount.gameObject.SetActive(true);
+        yield return new WaitForSeconds(time);
+        AmmoPickupAmount.gameObject.SetActive(false);
+        looting = false;
+        gameObject.SetActive(false);
     }
 }
